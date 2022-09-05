9a590a998483ebd294b7e542866cb204bb67afb2
ljcnh <1016821942@qq.com>
2022-09-05 14:32:00 +0800

    modify stag mysql

--- config/stag.py
+++ config/stag.py
@@ -36,16 +36,16 @@ if FRONTEND_BACKEND_SEPARATION:
     CORS_ORIGIN_ALLOW_ALL = True
     CORS_ALLOW_CREDENTIALS = True
 
-# DATABASES.update(
-#     {
-#         'default': {
-#             'ENGINE': 'django.db.backends.mysql',
-#             'NAME': '',  # 数据库名
-#             'USER': '',  # 数据库用户
-#             'PASSWORD': '',  # 数据库密码
-#             'HOST': '',  # 数据库主机
-#             'PORT': '3306',  # 数据库端口
-#         },
-#     }
-# )
+DATABASES.update(
+    {
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': 'demo',  # 数据库名
+            'USER': 'root',  # 数据库用户
+            'PASSWORD': '123456',  # 数据库密码
+            'HOST': '10.3.221.133',  # 数据库主机
+            'PORT': '3366',  # 数据库端口
+        },
+    }
+)
 
